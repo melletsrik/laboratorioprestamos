079c26c864c06889a431f2e5d25fecaf034f476a
melletsirk <melkrischf@gmail.com>
2025-06-25 12:51:03 -0400

    Correccion ruta usuarios

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const authRoutes = require('./auth.routes');
 const materialRoutes = require('./material.routes');
 const docenteRoutes = require('./docente.routes');
-const auxiliarRoutes = require('./usuario.routes'); //EN SI SON LAS RUTAS DE USUARIO
+const usuarioRoutes = require('./usuario.routes'); //EN SI SON LAS RUTAS DE USUARIO
 const estudianteRoutes = require('./estudiante.routes');
 const materiaRoutes = require('./materia.routes');
 const prestamoRoutes = require('./prestamo.routes')
@@ -15,7 +15,7 @@ router.use('/auth', authRoutes);
 // Rutas protegidas (requieren autenticación)
 router.use('/materiales', materialRoutes);
 router.use('/docentes', docenteRoutes);
-router.use('/auxiliares', auxiliarRoutes);
+router.use('/usuarios', usuarioRoutes);
 router.use('/estudiantes', estudianteRoutes);
 router.use('/materias',materiaRoutes);
 router.use('/prestamos',prestamoRoutes)
